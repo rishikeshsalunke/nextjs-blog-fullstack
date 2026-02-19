cf72c67270150d034f7c6b72d8d72af919fa8cf7
Rishikesh Salunke <rishisalunke14@gmail.com>
2026-02-19 14:30:12 +0530

    frontend for live deployment

--- backend/blog-backend/server.js
+++ backend/blog-backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // 5️⃣ Middlewares
 app.use(cors({
-  origin: process.env.CLIENT_URL || "https://nextjs-blog-fullstack-zeta.vercel.app/",
+  origin: process.env.CLIENT_URL || "https://nextjs-blog-fullstack-wheat.vercel.app/",
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
